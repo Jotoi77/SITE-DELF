1898a7c7266bd5347592b599dbaa06d7d332a8ca
Emerson Souza <140824904+Jotoi77@users.noreply.github.com>
2024-01-12 17:38:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Obtém a referência do botão pelo ID
+var Consulta = document.getElementById('Consulta');
+
+// Adiciona um ouvinte de eventos de clique ao botão
+Consulta.addEventListener('click', function() {
+  // Redireciona para o ponto específico da página
+  window.location.href = 'https://wa.link/2k49cm';
+});
+
+// Obtém a referência do botão pelo ID
+var Exames = document.getElementById('Exames');
+
+// Adiciona um ouvinte de eventos de clique ao botão
+Exames.addEventListener('click', function() {
+  // Redireciona para o ponto específico da página
+  window.location.href = '#especialidades';
+});
+
+// Obtém a referência do botão pelo ID
+var Contato = document.getElementById('Contato');
+
+// Adiciona um ouvinte de eventos de clique ao botão
+Contato.addEventListener('click', function() {
+  // Redireciona para o ponto específico da página
+  window.location.href = 'https://wa.link/2k49cm';
+});
+
+// Obtém a referência do botão pelo ID
+var Localização = document.getElementById('Localização');
+
+// Adiciona um ouvinte de eventos de clique ao botão
+Localização.addEventListener('click', function() {
+  // Redireciona para o ponto específico da página
+  window.location.href = 'https://www.google.com.br/maps/place/Delf+Medicina+Ocupacional/@-22.8646762,-43.2577858,17z/data=!3m1!4b1!4m6!3m5!1s0x997c004dbdd7fb:0xf5b7b03a7fb14c2c!8m2!3d-22.8646762!4d-43.2552109!16s%2Fg%2F1tfbzph7?entry=ttu';
+});
+
+// Obtém a referência do botão pelo ID
+var agendas = document.getElementById('agendas');
+
+// Adiciona um ouvinte de eventos de clique ao botão
+agendas.addEventListener('click', function() {
+  // Redireciona para o ponto específico da página
+  window.location.href = 'https://wa.link/2k49cm';
+});
